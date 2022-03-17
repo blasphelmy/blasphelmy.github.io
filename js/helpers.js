5266b522f0122ef22b564c529b205d6321510dbc
blasphemly <97411482+blasphemly@users.noreply.github.com>
2022-03-16 18:32:43 -0700

    styling rules and bug fixes

---
+++ js/helpers.js
@@ -0,0 +1,34 @@
+function stickyMenu(){
+    var contactBlockElement = document.getElementById("contactsBlock");
+    var dummyBlock = document.getElementsByClassName("dummy")[0];
+    var navMenuHeight = document.getElementById("navWrapper").offsetHeight;
+    if(this.window.scrollY >= navMenuHeight && window.innerWidth > 1100 && !contactBlockElement.classList.contains("fixContactBlock")){
+          setTimeout(() => {
+           contactBlockElement.classList.remove("KFfadeout");
+           contactBlockElement.classList.add("KFfadein"); 
+           setTimeout(() => {
+            contactBlockElement.classList.remove("KFfadein")
+           }, 75);
+        }, 75);
+        dummyBlock.style.display = "block";
+        contactBlockElement.classList.add("fixContactBlock", "KFfadeout");
+    }else if(this.window.scrollY < navMenuHeight && contactBlockElement.classList.contains("fixContactBlock")){
+        setTimeout(() => {
+           contactBlockElement.classList.remove("KFfadeout");
+           contactBlockElement.classList.add("KFfadein"); 
+           setTimeout(() => {
+            contactBlockElement.classList.remove("KFfadein")
+           }, 75);
+        }, 75);
+        dummyBlock.style.display = "none";
+        contactBlockElement.classList.remove("fixContactBlock");
+        contactBlockElement.classList.add("KFfadeout");
+    }else if(window.innerWidth <= 1100){
+        contactBlockElement.classList.remove("fixContactBlock");
+    }
+}
+function sendMail(){
+    var subjectText = document.getElementsByName("emailSubject")[0].value;
+    var bodyText = document.getElementsByName("emailBody")[0].value;
+    window.open('mailto:dhnguyen08@student.rtc.edu?subject=' + subjectText + '&body=' +  bodyText);
+}
